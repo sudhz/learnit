fa9228887b03dd26ac7585303de44b5be2e0382e
N Chandan <N.Chandan@ceridian.com>
2024-03-08 17:02:51 +0530

    added course description

--- learnit-frontend/learnit/src/components/CourseCatalogPage.tsx
+++ learnit-frontend/learnit/src/components/CourseCatalogPage.tsx
@@ -12,153 +12,153 @@ import { Link } from "react-router-dom";
 
 const courses = [
   {
-    id: 1,
-    title: "Introduction to React",
-    price: "$20.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 1,
+      "title": "Introduction to React",
+      "price": "$20.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 2,
-    title: "JavaScript Fundamentals",
-    price: "$15.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 2,
+      "title": "JavaScript Fundamentals",
+      "price": "$15.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 3,
-    title: "CSS Styling Techniques",
-    price: "$12.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 3,
+      "title": "CSS Styling Techniques",
+      "price": "$12.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 4,
-    title: "Responsive Web Design",
-    price: "$18.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 4,
+      "title": "Responsive Web Design",
+      "price": "$18.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 5,
-    title: "State Management in React",
-    price: "$25.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 5,
+      "title": "State Management in React",
+      "price": "$25.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 6,
-    title: "Building RESTful APIs",
-    price: "$22.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 6,
+      "title": "Building RESTful APIs",
+      "price": "$22.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 7,
-    title: "React Hooks Deep Dive",
-    price: "$30.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 7,
+      "title": "React Hooks Deep Dive",
+      "price": "$30.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 8,
-    title: "Advanced JavaScript Patterns",
-    price: "$28.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 8,
+      "title": "Advanced JavaScript Patterns",
+      "price": "$28.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 9,
-    title: "CSS Grid Layout",
-    price: "$23.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 9,
+      "title": "CSS Grid Layout",
+      "price": "$23.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 10,
-    title: "Sass for Beginners",
-    price: "$19.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 10,
+      "title": "Sass for Beginners",
+      "price": "$19.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 11,
-    title: "Understanding the DOM",
-    price: "$21.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 11,
+      "title": "Understanding the DOM",
+      "price": "$21.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 12,
-    title: "React Context API",
-    price: "$27.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 12,
+      "title": "React Context API",
+      "price": "$27.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 13,
-    title: "TypeScript for JavaScript Developers",
-    price: "$35.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 13,
+      "title": "TypeScript for JavaScript Developers",
+      "price": "$35.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 14,
-    title: "Web Performance Optimization",
-    price: "$32.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 14,
+      "title": "Web Performance Optimization",
+      "price": "$32.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 15,
-    title: "Modern CSS Layouts",
-    price: "$29.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 15,
+      "title": "Modern CSS Layouts",
+      "price": "$29.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 16,
-    title: "Asynchronous JavaScript",
-    price: "$24.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 16,
+      "title": "Asynchronous JavaScript",
+      "price": "$24.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 17,
-    title: "Accessibility in Web Design",
-    price: "$26.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 17,
+      "title": "Accessibility in Web Design",
+      "price": "$26.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 18,
-    title: "SEO Basics for Developers",
-    price: "$33.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 18,
+      "title": "SEO Basics for Developers",
+      "price": "$33.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 19,
-    title: "React Native Introduction",
-    price: "$38.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 19,
+      "title": "React Native Introduction",
+      "price": "$38.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 20,
-    title: "Vue.js Essentials",
-    price: "$31.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "id": 20,
+      "title": "Vue.js Essentials",
+      "price": "$31.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
   },
   {
-    id: 21,
-    title: "Svelte Essentials",
-    price: "$35.00",
-    imgUrl:
-      "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
-  },
-];
+      "id": 21,
+      "title": "Svelte Essentials",
+      "price": "$35.00",
+      "imgUrl": "https://instructor-academy.onlinecoursehost.com/content/images/2023/05/1-How-to-Create-an-Online-Course-Thumbnail.jpg",
+      "description": "This is a course description."
+  }
+]
 
 const CourseCatalogPage = () => {
   return (
