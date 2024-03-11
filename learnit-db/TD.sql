040e20430be7a6f50d7089a092ab5ac85360d62b
vishnuathreya28 <vishnu.athreya77@gmail.com>
2024-03-11 12:33:58 +0530

    data.sql has the table data, TD.sql has the table definitions

---
+++ learnit-db/TD.sql
@@ -0,0 +1,29 @@
+CREATE TABLE students (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    email NVARCHAR(255) NOT NULL UNIQUE,
+    phone NVARCHAR(10),
+    college NVARCHAR(255),
+    password NVARCHAR(255) NOT NULL
+);
+CREATE TABLE instructors (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    email NVARCHAR(255) NOT NULL UNIQUE,
+    phone NVARCHAR(10),
+    password NVARCHAR(255) NOT NULL
+);
+CREATE TABLE courses (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    i_id INT,
+    c_desc NVARCHAR(255), --course description
+    FOREIGN KEY (i_id) REFERENCES instructors(id)
+);
+CREATE TABLE Course_Student_Mapping (
+    C_Id INT,
+    S_Id INT,
+    PRIMARY KEY (C_Id, S_Id),
+    FOREIGN KEY (C_Id) REFERENCES Courses(Id),
+    FOREIGN KEY (S_Id) REFERENCES Students(Id)
+);
