18de5ddef992ac5113fba65d15542e0419005a0e
sudhz <shudhanshumakwana@gmail.com>
2024-03-13 23:26:05 +0530

    course catalog fetches courses from db

--- learnit-frontend/src/App.tsx
+++ learnit-frontend/src/App.tsx
@@ -1,11 +1,26 @@
-import { AppBar, Box, Container, IconButton, Toolbar } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+  Stack,
+  Tooltip,
+} from "@mui/material";
 import SchoolIcon from "@mui/icons-material/School";
-import "./App.css";
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AddIcon from "@mui/icons-material/Add";
 
-function App() {
+const App: React.FC = () => {
+  const location = useLocation();
+  const renderProfileLink = location.pathname.startsWith("/instructor/home");
+  const renderStudentLink = location.pathname.startsWith("/student/home");
+
   return (
     <>
       <header>
@@ -32,12 +47,126 @@ function App() {
             >
               Learnit
             </Typography>
+
+            {renderStudentLink && (
+              <>
+                <Box sx={{ flexGrow: 1 }} />
+                <Box sx={{ display: "flex", alignItems: "center" }}></Box>
+                <Tooltip title="Library">
+                  <IconButton
+                    size="large"
+                    color="inherit"
+                    component={Link}
+                    to="/courses"
+                  >
+                    <LibraryBooksIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Notifications">
+                  <IconButton
+                    size="large"
+                    color="inherit"
+                    component={Link}
+                    to="/student/home/notifications"
+                  >
+                    <NotificationsIcon />
+                  </IconButton>
+                </Tooltip>
+              </>
+            )}
+            {renderProfileLink && (
+              <>
+                <Tooltip title="Library">
+                  <IconButton
+                    size="large"
+                    color="inherit"
+                    component={Link}
+                    to="/courses"
+                    sx={{ marginLeft: "auto" }}
+                  >
+                    <LibraryBooksIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Add Course">
+                  <IconButton
+                    size="large"
+                    color="inherit"
+                    component={Link}
+                    to="/instructor/home/coursebuilder"
+                  >
+                    <AddIcon /> {/* Assuming you have an AddIcon imported */}
+                  </IconButton>
+                </Tooltip>
+
+                <Tooltip title="My Courses">
+                  <IconButton
+                    size="large"
+                    color="inherit"
+                    component={Link}
+                    to="/my-courses"
+                  >
+                    <BookmarkIcon /> {/* Using MyCourses icon */}
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Profile">
+                  <IconButton
+                    size="large"
+                    color="inherit"
+                    component={Link}
+                    to={
+                      location.pathname.startsWith("/instructor/home")
+                        ? "/instructor/home/myprofile"
+                        : "/student/home/myprofile"
+                    }
+                  >
+                    <AccountCircleIcon />
+                  </IconButton>
+                </Tooltip>
+              </>
+            )}
           </Toolbar>
         </AppBar>
       </header>
       <main>
-        <Box sx={{ pt: 5 }}>
-          <Outlet />
+        <Box height="100%" pt={7} sx={{ display: "flex" }}>
+          {location.pathname.startsWith("/student/home") && (
+            <Box
+              width="20%"
+              padding={2}
+              height={710}
+              sx={{ bgcolor: "#1976d2" }}
+            >
+              <Stack spacing={2} mt={5}>
+                <Typography
+                  variant="h5"
+                  component={Link}
+                  sx={{ color: "white", textDecoration: "none" }}
+                  to=""
+                >
+                  Profile
+                </Typography>
+                <Typography
+                  variant="h5"
+                  component={Link}
+                  sx={{ color: "white", textDecoration: "none" }}
+                  to=""
+                >
+                  My Courses
+                </Typography>
+                <Typography
+                  variant="h5"
+                  component={Link}
+                  sx={{ color: "white", textDecoration: "none" }}
+                  to=""
+                >
+                  Subscriptions
+                </Typography>
+              </Stack>
+            </Box>
+          )}
+          <Box sx={{ flexGrow: 1, padding: 2 }}>
+            <Outlet />
+          </Box>
         </Box>
       </main>
       <footer>
@@ -65,6 +194,6 @@ function App() {
       </footer>
     </>
   );
-}
+};
 
 export default App;
--- learnit-frontend/src/components/student/StudentHome.tsx
+++ learnit-frontend/src/components/student/StudentHome.tsx
@@ -1,7 +1,44 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Button, Container } from "@mui/material";
 
 const StudentHome = () => {
-  return <Box margin={20}>StudentHome</Box>;
+  const backgroundStyle = {
+    backgroundImage: `url("https://cdn.elearningindustry.com/wp-content/uploads/2022/01/shutterstock_525008128.jpg")`, // Replace with your actual image URL
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    minHeight: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const contentStyle = {
+    margin: "20px",
+    padding: "20px",
+    border: "1px solid grey",
+    borderRadius: "2px",
+    backgroundColor: "rgba(255, 255, 255, 0.8)", // Adjust the background color and opacity
+    textAlign: "center",
+  };
+
+  return (
+    <Box sx={backgroundStyle}>
+      <Container maxWidth="sm">
+        <Box sx={contentStyle}>
+          <Typography variant="h4" gutterBottom>
+            Welcome
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Here you can find all the resources that need to update yourself
+          </Typography>
+          <Button variant="contained" color="primary" size="large">
+            Start Learning
+          </Button>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
 
 export default StudentHome;
