77ac0b3ca7e8e4f7f6b4aa4bb94408ddf73af208
sudhz <shudhanshumakwana@gmail.com>
2024-03-13 16:23:28 +0530

    protected the routes, only logged in user can access certain pages

--- learnit-frontend/src/components/instructor/InstructorHome.tsx
+++ learnit-frontend/src/components/instructor/InstructorHome.tsx
@@ -1,10 +1,6 @@
 import { Box } from "@mui/material";
-import { useContext } from "react";
-import { AuthContext } from "../../services/context/auth/authContext";
 
 const InstructorHome = () => {
-  const { auth } = useContext(AuthContext);
-  console.log(auth);
   return <Box margin={20}>InstructorHome</Box>;
 };
 
